4d54a7f8ae097b41330ea9ea2b82ea9a001b01f2
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-19 17:15:49 +0100

    tested connection with api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
+ <test/>
 
-  </div>
+
 </template>
 
 <script>
+import axios from "axios";
+
+import {store} from "./data/store";
+import test from './components/TestComponent.vue';
   export default {
-    
+    components:{
+      test,
+    },
+    data(){
+      return{
+        store,
+        projects:[],
+      }
+    },
+    methods:{
+    getAllProjects(){
+      axios.get(store.apiUrl + "/projects", {params:{page: this.currentPage}}).then((resp)=>{
+        console.log(resp.data);
+        this.projects = resp.data.results.data;
+        this.currentPage = resp.data.results.current_page;
+        this.lastPage = resp.data.results.last_page;
+      })
+    },
+  },
+  mounted() {
+    this.getAllProjects();
+  }
   }
 </script>
 
