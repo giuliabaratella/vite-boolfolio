bb174ee2a9e17ffe4928205b7130f30b5a0fdd3b
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-22 15:28:41 +0100

    updated navbar / added fallback route in router / added NotFound page / updated SingleProject page with img + fallback route

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-<h1>Single Project</h1>
-<div>{{ project.title }}</div>
-    </div>
+<div v-if="project">
+<h1>{{ project.title }}</h1>
+<div class="w-50">
+    <img :src="store.imgPath + project.image" :alt="project.title">
+
+</div>
+</div>
 </template>
 
 <script>
@@ -13,14 +16,19 @@ import {store} from "../data/store";
         data(){
             return{
                 store,
-                project:{},
+                project:null,
             }
         },
         methods:{
             getProjectData(){
-                axios.get(`${this.store.apiUrl}/projects/${this.$route.params.slug}`).then((resp)=>{
-        console.log(resp.data);
-        this.project = resp.data.results;
+                axios.get(`${this.store.apiUrl}${this.store.endpoints.projects}/${this.$route.params.slug}`).then((resp)=>{
+                console.log(resp.data);
+                if(resp.data.results){
+                    this.project = resp.data.results;
+
+                }else{
+                    this.$router.push({name: 'not-found'});
+                }
             })  
          }
         },
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AppHome from './pages/AppHome.vue'
 import AppProjects from './pages/AppProjects.vue'
 import SingleProject from './pages/SingleProject.vue'
+import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,14 +22,12 @@ const router = createRouter({
       name: 'project',
       component: SingleProject
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: "/:pathMatch(.*)*",
+      name: 'not-found',
+      component: NotFound
+    },
+
     
   ]
 })
