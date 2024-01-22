7002cd2dff68c5e1c99bc631949149e0b3413c3c
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-22 15:29:39 +0100

    updated store with img path

--- src/data/store.js
+++ src/data/store.js
@@ -6,6 +6,7 @@ export const store = reactive({
       projects: '/projects',
       categories: '/categories',
       technologies: '/technologies',
-   }
+   },
+   imgPath: "http://127.0.0.1:8000/storage/",
 
 });
